ba9c1942a8d8edabc5364f38a6769fcdb5f79ae0
longjinyuan <longjinyuan@51talk.com>
2021-09-24 16:31:27 +0800

    add init files

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by longjinyuan on 2021/9/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Hello World!" << endl;
+    return 0;
+}
